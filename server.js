3392f585a48d4fd82edee650d98e643ac986a690
Tim Brady <tfb414@gmail.com>
2017-08-05 15:19:15 -0400

    begins working on serving up a page with the temperature of my sensor

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
 
 var util = require("util");
-var tempData = {}
+const ws = require('ws');
+
 var spawn = require("child_process").spawn;
 var Promise = require('es6-promise').Promise;
 
+const server = new ws.Server({port: 8080});
+
+server.on('connection', (socket)=>{
+    console.log('someone connected!');
+    console.log(temp);
+    socket.on('message', (msg)=>{
+        console.log("They said: " + msg);
+        server.clients.forEach((client)=> {
+            socket.send("I know you are but what am I?");
+            //this sends it to every website that has this open
+        })
+        
+    });
+});
+
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     //do stuff
+//     res.end('<h1>' + currentTemp + '</h1>');
+//     //this is a method and it ends the convo and send it off
+// });
+
+// server.listen(8080);
+
+
 util.log('Working...')
 
+var tempData = {}
+var currentTemp;
+
 function runFile(){
+    
     var processSensor = spawn('python',["sensor.py"]);
     processSensor.stdout.on('data',function(chunk){
         var textChunk = chunk.toString('utf8');
@@ -15,9 +45,9 @@ function runFile(){
         var humid = Number(numbers[1]).toFixed(2);
         var date = Date.now()
         addToData(date, temp, humid);
-        return tempData;
+        currentTemp = temp;
+        console.log(currentTemp);
     });
-    console.log(tempData)
 ;};
 
 
@@ -29,10 +59,12 @@ function addToData(date, temp, humid){
     tempData[date] = {};
     tempData[date]['Temperature'] = temp
     tempData[date]['Humidity'] = humid
-    
-    
+    console.log(tempData);
 }
-
 
 setInterval(runFile, 5000);
 
+
+
+//how to do promises with the .on
+//why can't i use finder to see these files
